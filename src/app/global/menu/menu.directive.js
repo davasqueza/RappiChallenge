ae54ada23e60ece2334713d6d67a327f3a6f7e2c
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2018-10-27 23:18:00 -0500

    Menu load based on categories.json

--- src/app/global/menu/menu.directive.js
+++ src/app/global/menu/menu.directive.js
@@ -13,7 +13,50 @@
         toggle: "&",
         items: "="
       },
-      link: function () {}
+      link: function (scope) {
+        var path = [];
+        scope.menuItems = scope.items;
+
+        scope.selectMenu = selectMenu;
+        scope.goBack = goBack;
+        scope.redirectToCategory = redirectToCategory;
+
+        function selectMenu(menu) {
+          if(menu.sublevels){
+            path.push(menu.id);
+            scope.menuItems = menu.sublevels;
+            scope.currentMenu = menu;
+
+          }
+          else {
+            redirectToCategory(menu);
+          }
+        }
+
+        function goBack() {
+          path.pop();
+
+          scope.currentMenu = undefined;
+          scope.menuItems = scope.items;
+
+          _.each(path, function (menuID) {
+            scope.currentMenu = _.find(scope.menuItems, function (menu) {
+              return menu.id === menuID;
+            });
+            scope.menuItems = scope.currentMenu.sublevels
+          });
+        }
+
+        function redirectToCategory(menu) {
+          scope.currentMenu = undefined;
+          scope.menuItems = scope.items;
+          scope.toggle();
+
+          path = [];
+
+          //Redirigir a la categoría
+        }
+      }
     };
   }
 })();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -5,8 +5,7 @@
     .module('App')
     .controller('MainController', MainController);
 
-  /** @ngInject */
-  function MainController($mdSidenav) {
+  function MainController(MainService, $mdSidenav, $log, toastr) {
     var vm = this;
 
     vm.toggleMenu = toggleMenu;
@@ -14,5 +13,18 @@
     function toggleMenu() {
       $mdSidenav('left').toggle();
     }
+
+    function loadMenuItems() {
+      MainService.getAllCategories()
+        .then(function (result) {
+          vm.menuItems = result.categories;
+        })
+        .catch(function (error) {
+          toastr.error('Hubo un error al intentar cargar el menú, inténtalo nuevamente más tarde', 'Error');
+          $log.warn("Error while trying to load menu items", error.message);
+        });
+    }
+
+    loadMenuItems();
   }
 })();
