c6f4275393845b7619f309b0bbb97207fb5f7a52
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2018-10-28 02:12:27 -0500

    Implemented product loading by category ID

--- src/app/category/category.controller.js
+++ src/app/category/category.controller.js
@@ -6,9 +6,22 @@
     .controller('CategoryController', CategoryController);
 
   /** @ngInject */
-  function CategoryController() {
+  function CategoryController(MainService, $routeParams, toastr, $log) {
     var vm = this;
 
+    function loadProducts() {
+      var currentCategory = Number($routeParams.categoryPath.split("/").pop());
 
+      MainService.getProductsByCategoryID(currentCategory)
+        .then(function (products) {
+          vm.products = products;
+        })
+        .catch(function (error) {
+          toastr.error('Hubo un error al intentar cargar los productos de la categoría', 'Error');
+          $log.warn("Error while trying to load menu items", error.message);
+        });
+    }
+
+    loadProducts();
   }
 })();
