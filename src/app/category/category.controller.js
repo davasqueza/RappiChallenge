f691886511c1e9f4c36e61bd927ac30d74b3d34e
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2018-11-04 14:06:06 -0500

    Implemented sort by filter

--- src/app/category/category.controller.js
+++ src/app/category/category.controller.js
@@ -9,6 +9,15 @@
   function CategoryController(MainService, $routeParams, toastr, $log) {
     var vm = this;
 
+    vm.sortBy = [
+      {label: "Menor precio", sortExpression: "+price"},
+      {label: "Mayor precio", sortExpression: "-price"},
+      {label: "Menor cantidad", sortExpression: "+quantity"},
+      {label: "Mayor cantidad", sortExpression: "-quantity"},
+      {label: "Disponibles primero", sortExpression: "-available"},
+      {label: "Agotados primero", sortExpression: "+available"}
+    ];
+
     function loadProducts() {
       var currentCategory = Number($routeParams.categoryPath.split("/").pop());
 
