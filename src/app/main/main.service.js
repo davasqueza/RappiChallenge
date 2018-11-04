4daf28cd0130b1e8df74dc6a8add12a165edcf03
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2018-11-04 18:31:31 -0500

    Implemented cart summary services

--- src/app/main/main.service.js
+++ src/app/main/main.service.js
@@ -5,16 +5,21 @@
     .module('App')
     .service('MainService', MainService);
 
-  function MainService($resource) {
+  function MainService($resource, $log) {
     var service = {
       getAllCategories: getAllCategories,
-      getProductsByCategoryID: getProductsByCategoryID
+      getProductsByCategoryID: getProductsByCategoryID,
+      getCartSummary: getCartSummary,
+      addToCart: addToCart,
+      removeToCart: removeToCart
     };
 
     var resource = {
       categories: $resource("./assets/data/categories.json"),
       products: $resource("./assets/data/products.json")
     };
+
+    var cartSummary;
 
     return service;
 
@@ -28,6 +33,62 @@
           return product.sublevel_id === categoryID;
         })
       });
+    }
+
+    function getCartSummary() {
+      if(cartSummary){
+        return cartSummary;
+      }
+
+      if(!localStorage.cartSummary){
+        cartSummary = [];
+      }
+      else{
+        loadCartSummary();
+      }
+
+      return cartSummary;
+    }
+
+    function loadCartSummary() {
+      try {
+        cartSummary = JSON.parse(localStorage.cartSummary);
+      }
+      catch (err){
+        localStorage.removeItem("cartSummary");
+        $log.warn("Error loading previous cart summary: ", err);
+
+        cartSummary = [];
+      }
+    }
+
+    function addToCart(productID, amount) {
+      var product = _.find(cartSummary, function (product) {
+        return product.id = productID;
+      });
+
+      if(product){
+        product.amount = amount;
+      }
+      else{
+        cartSummary.push({id: productID, amount: amount});
+      }
+
+      saveCartSummary();
+    }
+
+    function removeToCart(productID) {
+      var productIndex = _.findIndex(cartSummary, function (product) {
+        return product.id = productID;
+      });
+
+      if(productIndex !== -1){
+        cartSummary.splice(productIndex, 1);
+      }
+    }
+
+    function saveCartSummary() {
+      localStorage.cartSummary = JSON.stringify(cartSummary);
     }
   }
 })();
